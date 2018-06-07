bf20344713f2f3b751564648b7c45b68287a731d
esatgozcu <35576161+esatgozcu@users.noreply.github.com>
2018-06-07 05:55:08 +0300

    Add files via upload

---
+++ LCDEkranKullanimi-1.ino
@@ -0,0 +1,15 @@
+#include <LiquidCrystal.h>
+
+// lcd (rs,e,d4,d5,d6,d7);
+
+LiquidCrystal lcd(8,7,6,5,4,3);
+
+void setup() {
+
+  lcd.begin(16,2);
+  lcd.print("Game Over !!!");
+}
+
+void loop() {
+
+}
